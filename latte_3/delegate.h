01029a8d7d654c873b81cbbcd5e664bfec011062
Raymond ZHAO <raymond.zhao@monash.edu>
2022-05-02 14:21:57 +1000

    Revert "Make Delegate deterministic"
    This reverts commit 5baaca8b85ecf4305e295622508108262cdc4298.

--- latte_3/delegate.h
+++ latte_3/delegate.h
@@ -11,6 +11,6 @@
 #include "poly.h"
 #include "mat.h"
 
-void delegate(MAT_FFT *s_fft, MAT_FFT *s_tree_root, POLY_FFT *s_tree_dim2, const MAT_FFT *fft_basis, const POLY_64 *a, const uint64_t l, const MAT_FFT *tree_root, const POLY_FFT *tree_dim2);
+void delegate(MAT_FFT *s_fft, MAT_FFT *s_tree_root, POLY_FFT *s_tree_dim2, const MAT_FFT *fft_basis, const POLY_64 *a, const uint64_t l, const unsigned char *seed, const MAT_FFT *tree_root, const POLY_FFT *tree_dim2);
 
 #endif
